b4e76bd5c89a258574b2b11602178fe7a0842c34
zhangwenxue <734781246@qq.com>
2021-02-20 16:41:41 +0800

    zhangwenxue
    init

--- Example/Pods/BOCeUpImageManager/README.md
+++
@@ -1,2 +0,0 @@
-# BOCeUpImageManager
-A Image UpLoadingManager
--- Example/Pods/Target Support Files/BOCeNetManager/BOCeNetManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BOCeNetManager : NSObject
-@end
-@implementation PodsDummy_BOCeNetManager
-@end
